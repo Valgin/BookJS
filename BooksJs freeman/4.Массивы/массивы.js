81dc972aab8aeadcdff4c40aa43c2512d8ec42d6
Владимир <valgin.vladimir@gmail.com>
2024-07-05 17:02:01 +0300

    3

---
+++ BooksJs freeman/4.Массивы/массивы.js
@@ -0,0 +1,42 @@
+//объявление массива чисеал
+var arr [1, 2, 3, 4, 5];
+
+// массив строк
+
+var flavors = ["vanilla", "butterscotch", "lavender", "chocolate", "cookie dough"];
+
+// обращение к массиву
+var flavorOfTheDay = flavors[2];
+
+
+//как получить количество элемнтов в массиве (length)
+
+// нужно указать имя массива и имя свойства(length)
+
+var numFlavirs = flavors.length;
+
+//узнаем индекс последнего элемента в массиве 
+
+myArray[myArray.length - 1];
+
+
+//создание пустого массива
+
+var genres = [];
+
+genres[0] = "Rockabilly";
+genres[1] = "Ambient";
+
+
+//будет содержать значение 2 - длину массива
+var size = genres.length;
+
+//команда push добавляет новые элемнты при котором не нужно
+// беспокоиться об индексе
+
+var genres = [];
+
+genres.push("Rockabilly");
+genres.push("Ambient");
+
+var size = genres.length;
---
+++ BooksJs freeman/4.Массивы/перебор.js
@@ -0,0 +1,42 @@
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
+    34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
+    46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+
+
+    
+function printAndGetHighScore(scores) {
+
+    var highScore = 0;
+    var output;
+
+    for (var i = 0; i < scores.length; i++){
+        output = "Bubble solution #" + i + " score: " + scores[i];
+        console.log(output);
+        if(scores[i] > highScore) {
+            highScore = scores[i];
+
+
+        }
+    }
+
+    return highScore;
+
+}
+
+var highScore = printAndGetHighScore(scores);
+console.log("Bubbles test: " + scores.length);
+console.log("Highest bubble score: " + highScore);
+
+
+var bestSolution = [];
+
+for (var i = 0; i < scores.length; i++){
+    if(scores[i] == highScore) {
+        bestSolution.push(i);
+    }
+}
+
+console.log("Solution with the highest score: " + bestSolution);
+
+
+//191
